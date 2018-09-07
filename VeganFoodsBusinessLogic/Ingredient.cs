819ee97abdf1b6d3e1d3aab1649c3172cb7615ad
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-09-07 14:01:46 +0200

    Added a Recipe class and an Ingredient class to the business logic

---
+++ VeganFoodsBusinessLogic/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VeganFoodsBusinessLogic
+{
+    class Ingredient
+    {
+        public int IngredientID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Ingredient(int ingredientID, string name, string type)
+        {
+            IngredientID = ingredientID;
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ VeganFoodsBusinessLogic/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VeganFoodsBusinessLogic
+{
+    class Recipe
+    {
+        public int RecipeID { get; set; }
+        public int RecipeIngredientsID { get; set; }
+        public string Name { get; set; }
+
+        public Recipe(int recipeID, int recipeIngredientsID, string name)
+        {
+            RecipeID = recipeID;
+            RecipeIngredientsID = recipeIngredientsID;
+            Name = name;
+        }
+    }
+}
