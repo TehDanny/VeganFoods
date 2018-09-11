aa5d4a81530dc2b4bdeb82fef19789b1f6520f27
Daniel Alexander Nedergaard <daniel.nedergaard@uptime.dk>
2018-09-11 10:44:08 +0200

    Added a connectionstring to mysql database

--- VeganFoodsBusinessLogic/DataController.cs
+++ VeganFoodsBusinessLogic/DataController.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace VeganFoodsBusinessLogic
 {
     class DataController : IDataController
     {
+        private MySqlConnection connectionString;
+
+        public DataController()
+        {
+            connectionString = new MySqlConnection("Server=localhost;database=VeganFoods;uid=root;Pwd=admin");
+        }
+
         public void CreateRecipe(Recipe recipe)
         {
             throw new NotImplementedException();
