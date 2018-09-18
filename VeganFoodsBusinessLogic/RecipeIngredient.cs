9c8da1cb94362c0cf3f4d4e9dc1a5e702ca1bdc5
Daniel Alexander Nedergaard <danielxnedergaard@hotmail.dk>
2018-09-18 14:43:50 +0200

    Added a RecipeIngredient class

---
+++ VeganFoodsBusinessLogic/RecipeIngredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeganFoodsBusinessLogic
+{
+    class RecipeIngredient
+    {
+        public int RecipeID { get; set; }
+        public int IngredientID { get; set; }
+        public int Measurement { get; set; }
+        public string MeasurementType { get; set; }
+
+        public RecipeIngredient()
+        {
+
+        }
+
+        public RecipeIngredient(int recipeID, int ingredientID, int measurement, string measurementType)
+        {
+            RecipeID = recipeID;
+            IngredientID = ingredientID;
+            Measurement = measurement;
+            MeasurementType = measurementType;
+        }
+    }
+}
